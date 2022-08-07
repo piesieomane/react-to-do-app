d764f57309fbe84d02a9b4de5facbc3ddd7b258a
piesieomane <piesieyeboah@gmail.com>
2022-08-07 20:30:23 +0300

    edit todos item

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -60,6 +60,17 @@ export class TodoContainer extends Component {
     });
   };
 
+  setUpdate = (updatedTitle, id) => {
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.title = updatedTitle;
+        }
+        return todo;
+      }),
+    });
+  };
+
   render() {
     return (
       <div className='container'>
@@ -70,6 +81,7 @@ export class TodoContainer extends Component {
             todos={this.state.todos}
             handleChangeProps={this.handleChange}
             deleteTodoProps={this.delTodo}
+            setUpdate={this.setUpdate}
           />
         </div>
       </div>
